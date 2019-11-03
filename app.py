ff5b46a26f488dca6d59dc70011d46aee89840aa
josephineprickett <57304743+josephineprickett@users.noreply.github.com>
2019-11-02 20:34:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 #Method will automatically execute when our endpoint receives a POST call
 @app.route('/', methods=['POST'])
 def msg_received_from_group():
-
+  send_msg("we are")	
   #Format the data we receive as a JSON
   data = request.get_json()
   log('{}'.format(data))
